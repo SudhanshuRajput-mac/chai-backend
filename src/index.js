f2b2938c9dbb2129511a5f4c60d34761c79cb293
SudhanshuRajput-mac <sudhanshu678rajput@gmail.com>
2026-06-22 17:47:40 +0530

    connect to DataBase

---
+++ src/index.js
@@ -0,0 +1,47 @@
+// Second Apporach 
+// require('dotenv').config({path: './env'})
+
+// using import statement 
+
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB() ; 
+
+
+
+
+// Apporach First
+/*
+import mongoose from "mongoose"
+
+import { DB_NAME } from "./constants"
+
+import express from "express"
+
+const app = express()
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+        app.on("error" , (error)=>{
+            console.log("Error : " , error);
+            throw error ; 
+        })
+
+        app.listen(process.env.PORT , () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("Error occured in db connection : ",error)
+    }
+})()
+
+*/
